074aa8fa8583b4d253aa8029889bf8e71e9cb91a
ezm-dev <ezm.sabry@gmail.com>
2026-08-08 16:11:54 +1000

    feat: add footer component with copyright information

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Footer(){
+    return (   
+      <footer className="bg-gray-800 text-white py-4 px-8 mt-auto">
+        <div className="container mx-auto text-center">
+          <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+        </div>
+      </footer>
+    )
+}
+
